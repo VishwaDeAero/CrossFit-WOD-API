57a4c5bb6013a51f088a6e391920c31043c4e498
vishwa de aero <vishwadeaero@gmail.com>
2023-09-07 23:55:41 +0530

    connect route with controller

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const app = express()
 const port = process.env.PORT || 3000;
 
 // Display
-app.get('/', (req, res) => {
-    res.send('CrossFit WOD ExpressJs API')
-});
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 // Listen
 app.listen(port, () => {
---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { getAllWorkouts, getOneWorkout, createNewWorkout, updateOneWorkout, deleteOneWorkout } = require('../../controllers/workoutContorller');
+const router = express.Router();
+
+router.get("/", getAllWorkouts);
+
+router.get("/:workoutId", getOneWorkout);
+
+router.post("/", createNewWorkout);
+
+router.patch("/:workoutId", updateOneWorkout);
+
+router.delete("/:workoutId", deleteOneWorkout);
+
+module.exports = router;
